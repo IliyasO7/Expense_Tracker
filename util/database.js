730615661893de22b21e7274306b7568d11981c3
7083375809 <shaikhiliyas343>
2022-10-31 16:28:40 +0530

    DataBase of Expense

---
+++ util/database.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('expense-node-complete','root','Iliyastechs@ngli10', {
+    dialect: 'mysql',
+    host: 'localhost'
+    });
+
+
+    
+    //connecting sequelize to db create connection pool
+
+   
+module.exports = sequelize;
